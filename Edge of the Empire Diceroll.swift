de8d67d761d3353b647a75b3ea94dc7f93a1afad
Counter-Weight <danielwatson.311@gmail.com>
2016-12-20 17:42:45 -0800

    refigure core logic: switch to directly counting rolls

--- Edge of the Empire Diceroll.swift
+++ Edge of the Empire Diceroll.swift
@@ -1,311 +1,267 @@
-import Darwin // for arc4random_uniform
-import Foundation
+import Darwin.C.math // for arc4random_uniform
 
-let start0 = Date()
+private var blanks = false // used only when the roll is completely blank
+private var successes = 0
+private var failures = 0
+private var advantages = 0
+private var threats = 0
+private var triumphs = 0 // critical successes
+private var despairs = 0 // critical failures
+private var lights = 0
+private var darks = 0
 
-enum Symbol { // state values for dice faces
-	// case blank // removed to simplify calculations
-	case success
-	case failure
-	case advantage
-	case threat
-	case triumph 	// critical_success
-	case despair	// critical_failure
-	case light
-	case dark
-}
+// arc4random_uniform gives a result from 0 to below the number given as an argument
+// so to match dice values, a 1 must be added to the function's output
+private let random: (UInt32) -> UInt32 = { arc4random_uniform($0) + 1 }
 
-/* All The Die Types */
-func forceDie() -> [Symbol] {
-	let num = arc4random_uniform(12) + 1
-
-	switch num {
-	case 1...6:
-		return [.dark]
-	case 7:
-		return [.dark]
-	case 8, 9:
-		return [.light]
-	case 10...12:
-		return [.light, .light]
-	default:
-		return []
+// =================== Begin - All The Die Types =================== //
+private func forceDie(_ num: Int) {
+	for _ in 0..<num {
+		switch random(12) {
+		case 1...6:
+			darks += 1
+		case 7:
+			darks += 1
+		case 8, 9:
+			lights += 1
+		case 10...12:
+			lights += 2
+		default:
+			print("Error: errant random number")
+			exit(1)
+		}
 	}
 }
 
-func challengeDie() -> [Symbol] {
-	let num = arc4random_uniform(12) + 1
-
-	switch num {
-	case 1:
-		return []
-	case 2, 3:
-		return [.failure]
-	case 4, 5:
-		return [.failure, .failure]
-	case 6, 7:
-		return [.threat]
-	case 8, 9:
-		return [.threat, .failure]
-	case 10, 11:
-		return [.threat, .threat]
-	case 12:
-		return [.despair]
-	default:
-		return []
+private func challengeDie(_ num: Int) {
+	for _ in 0..<num {
+		switch random(12) {
+		case 1:
+			blanks = true
+		case 2, 3:
+			failures += 1
+		case 4, 5:
+			failures += 2
+		case 6, 7:
+			threats += 1
+		case 8, 9:
+			threats += 1; failures += 1
+		case 10, 11:
+			threats += 2
+		case 12:
+			despairs += 1
+		default:
+			print("Error: errant random number")
+			exit(1)
+		}
 	}
 }
 
-func proficiencyDie() -> [Symbol] {
-	let num = arc4random_uniform(12) + 1
-
-	switch num {
-	case 1:
-		return []
-	case 2, 3:
-		return [.success]
-	case 4, 5:
-		return [.success, .success]
-	case 6:
-		return [.advantage]
-	case 7...9:
-		return [.advantage, .success]
-	case 10, 11:
-		return [.advantage, .advantage]
-	case 12:
-		return [.triumph]
-	default:
-		return []
+private func proficiencyDie(_ num: Int) {
+	for _ in 0..<num {
+		switch random(12) {
+		case 1:
+			blanks = true
+		case 2, 3:
+			successes += 1
+		case 4, 5:
+			successes += 2
+		case 6:
+			advantages += 1
+		case 7...9:
+			advantages += 1; successes += 1
+		case 10, 11:
+			advantages += 2
+		case 12:
+			triumphs += 1
+		default:
+			print("Error: errant random number")
+			exit(1)
+		}
 	}
 }
 
-func difficultyDie() -> [Symbol] {
-	let num = arc4random_uniform(8) + 1
-
-	switch num {
-	case 1:
-		return []
-	case 2:
-		return [.failure]
-	case 3:
-		return [.failure, .failure]
-	case 4...6:
-		return [.threat]
-	case 7:
-		return [.threat, .threat]
-	case 8:
-		return [.threat, .failure]
-	default:
-		return []
+private func difficultyDie(_ num: Int) {
+	for _ in 0..<num {
+		switch random(8) {
+		case 1:
+			blanks = true
+		case 2:
+			failures += 1
+		case 3:
+			failures += 2
+		case 4...6:
+			threats += 1
+		case 7:
+			threats += 2
+		case 8:
+			threats += 1; failures += 1
+		default:
+			print("Error: errant random number")
+			exit(1)
+		}
 	}
 }
 
-func abilityDie() -> [Symbol] {
-	let num = arc4random_uniform(8) + 1
-
-	switch num {
-	case 1:
-		return []
-	case 2, 3:
-		return [.success]
-	case 4:
-		return [.success, .success]
-	case 5, 6:
-		return [.advantage]
-	case 7:
-		return [.success, .advantage]
-	case 8:
-		return [.advantage, .advantage]
-	default:
-		return []
+private func abilityDie(_ num: Int) {
+	for _ in 0..<num {
+		switch random(8) {
+		case 1:
+			blanks = true
+		case 2, 3:
+			successes += 1
+		case 4:
+			successes += 2
+		case 5, 6:
+			advantages += 1
+		case 7:
+			successes += 1; advantages += 1
+		case 8:
+			advantages += 2
+		default:
+			print("Error: errant random number")
+			exit(1)
+		}
 	}
 }
 
-func setBackDie() -> [Symbol] {
-	let num = arc4random_uniform(6) + 1
-
-	switch num {
-	case 1, 2:
-		return []
-	case 3, 4:
-		return [.failure]
-	case 5, 6:
-		return [.threat]
-	default:
-		return []
+private func setBackDie(_ num: Int) {
+	for _ in 0..<num {
+		switch random(6) {
+		case 1, 2:
+			blanks = true
+		case 3, 4:
+			failures += 1
+		case 5, 6:
+			threats += 1
+		default:
+			print("Error: errant random number")
+			exit(1)
+		}
 	}
 }
 
-func boostDie() -> [Symbol] {
-	let num = arc4random_uniform(6) + 1
-
-	switch num {
-	case 1, 2:
-		return []
-	case 3:
-		return [.advantage, .advantage]
-	case 4:
-		return [.advantage]
-	case 5:
-		return [.advantage, .success]
-	case 6:
-		return [.success]
-	default:
-		return []
+private func boostDie(_ num: Int) {
+	for _ in 0..<num {
+		switch random(6) {
+		case 1, 2:
+			blanks = true
+		case 3:
+			advantages += 2
+		case 4:
+			advantages += 1
+		case 5:
+			advantages += 1; successes += 1
+		case 6:
+			successes += 1
+		default:
+			print("Error: errant random number")
+			exit(1)
+		}
 	}
 }
-/* End Of The Die Types */
+// =================== End - All The Die Types =================== //
+
+private func usage() {
+	print("USAGE: <command> [Number][Type] [Number][Type] ...")
+	print()
+	print("\t" + "type: use only the first character of the die type's name")
+	print("\t" + "e.g. `<command> 2b 3a` for 2 Boost dice and 3 Ability dice")
+	print()
+	print("INPUT:", CommandLine.arguments.dropFirst())
+
+	exit(1)
+}
 
 // determines which die to roll
-func rollDice(numberOfDice number: Int, ofType type: Character) -> [Symbol] {
-	let thing : () -> [Symbol]
+private func rollDice(number num: Int, ofType type: Character) {
 	switch type {
 	case "f":
-		thing = forceDie
+		forceDie(num)
 	case "c":
-		thing = challengeDie
+		challengeDie(num)
 	case "p":
-		thing = proficiencyDie
+		proficiencyDie(num)
 	case "d":
-		thing = difficultyDie
+		difficultyDie(num)
 	case "a":
-		thing = abilityDie
+		abilityDie(num)
 	case "s":
-		thing = setBackDie
+		setBackDie(num)
 	case "b":
-		thing = boostDie
+		boostDie(num)
 	default:
 		print("`\(type)`, is not a valid dice type, the final character of an argument must be the type of dice")
-		return []
+		usage()
 	}
-
-	var result = [Symbol]()
-	for _ in 0..<number {
-		result += thing()
-	}
-	return result
 }
+
+private var notZeros: Bool { return successes > 0 || failures > 0 || advantages > 0 ||
+	threats > 0 || triumphs > 0 || despairs > 0 || lights > 0 || darks > 0 }
 
 // outputs the results of a roll
-func output() {
-	let start2 = Date()
-		// continue unlesss no die were rolled or only blanks were rolled
-		guard !roll.isEmpty else {
-			print("Blank/neutral roll")
-			let end2 = Date()
-			print("blank: \(end2.timeIntervalSince(start2))")
-			return
-		}
-		// tried checking against an array only of blanks but at large sizes it loses performance
-		/*guard roll.contains(where: {$0 != }) else {
-			print("Blank/neutral roll")
-			let end2 = Date()
-			print("blank: \(end2.timeIntervalSince(start2))")
-			return
-		}*/
-	let end2 = Date()
-	print("blank: \(end2.timeIntervalSince(start2))")
+private func output() {
+	if successes + triumphs - failures - despairs > 0 {
+		print("===== SUCCESS! =====")
+	} else {
+		print("===== FAILURE! =====")
+	}
 
-	// counts occurrences by counting the elements in a new array formed only of the matching items in `roll`
-	var start = Date()
-		var successes = 0,
-			failures = 0,
-			advantages = 0,
-			threats = 0,
-			light = 0,
-			dark = 0,
-			triumphs = 0,
-			despairs = 0
+	// continue unlesss no die were rolled or only blanks were rolled
+	guard notZeros else {
+		print("Only blanks were rolled")
+		exit(0)
+	}
 
-		for dieFace in roll {
-			switch dieFace {
-			case .success:
-				successes += 1
-			case .failure:
-				failures += 1
-			case .advantage:
-				advantages += 1
-			case .threat:
-				threats += 1
-			case .light:
-				light += 1
-			case .dark:
-				dark += 1
-			case .triumph:
-				triumphs += 1
-			case .despair:
-				despairs += 1
-			}
-		}
-	var end = Date()
-	print("count: \(end.timeIntervalSince(start))")
-
-	start = Date()
-		if successes + triumphs - despairs - threats > 0 {
-			print("SUCCESS!")
-		} else {
-			print("FAILURE!")
-		}
-		// Success/Failure
-		if successes > 0 {
-			print("\(successes) Success"/* + (successes > 1 ? "es" : "")*/)
-		}
-		if failures > 0 {
-			print("\(failures) Failure"/* + (failures > 1 ? "s" : "")*/)
-		}
-		// Advantage/Threat
-		if advantages > 0 {
-			print("\(advantages) Advantage"/* + (advantages > 1 ? "s" : "")*/)
-		}
-		if threats > 0 {
-			print("\(threats) Threat"/* + (threats > 1 ? "s" : "")*/)
-		}
-		// Triumph/Despair
-		if triumphs > 0 {
-			print("\(triumphs) Triumph"/* + (triumphs > 1 ? "s" : "")*/)
-		}
-		if despairs > 0 {
-			print("\(despairs) Despair"/* + (despairs > 1 ? "s" : "")*/)
-		}
-		// Light/Dark
-		if light > 0 {
-			print("\(light) Light Side"/* + (light > 1 ? "s" : "")*/)
-		}
-		if dark > 0 {
-			print("\(dark) Dark Side"/* + (dark > 1 ? "s" : "")*/)
-		}
-	end = Date()
-	print("output: \(end.timeIntervalSince(start))")
+	// Successes/Failures
+	let total = successes - failures
+	if total > 0 {
+		print("\(total) Successes"/* + (successes > 1 ? "es" : "")*/)
+	} else if total < 0 {
+		print("\(-total) Failures"/* + (successes > 1 ? "s" : "")*/)
+	}
+	// Advantages/threats
+	let total_2 = advantages - threats
+	if total_2 > 0 {
+		print("\(total_2) Advantages"/* + (advantages > 1 ? "s" : "")*/)
+	} else if total_2 < 0 {
+		print("\(-total_2) Threats"/* + (threats > 1 ? "s" : "")*/)
+	}
+	// Triumphs/Despair
+	if triumphs > 0 {
+		print("\(triumphs) Triumphs"/* + (triumphs > 1 ? "s" : "")*/)
+	}
+	if despairs > 0 {
+		print("\(despairs) Despairs"/* + (despairs > 1 ? "s" : "")*/)
+	}
+	// Lights/Darks
+	if lights > 0 {
+		print("\(lights) Light Sides"/* + (lights > 1 ? "s" : "")*/)
+	}
+	if darks > 0 {
+		print("\(darks) Dark Sides"/* + (darks > 1 ? "s" : "")*/)
+	}
 }
 
-// let start1 = Date()
-var roll = [Symbol]()
-//parses the command line argument for the number and type of dice to roll
-let start = Date()
-	for dicePool in CommandLine.arguments.dropFirst() { // second argument and onward, because the first argument seems to be the call to the file/command
-		// let indexOfType = dicePool.index(before: arg.endIndex) // assuming the input was given as: dice number, dice type ("2a"). the last character should be the dice type and all previous characters form the number of dice
-		guard let diceType = dicePool.characters.last else {
-			print("no dice-type was specified")
-			continue
-		} // should be dice type
+// parses the command line arguments for the number and type of dice to roll
+for arg in CommandLine.arguments.dropFirst() { // second argument and onward, because the first argument seems to be the call to the file/command
+	var dicePool = arg.characters
 
-		/*guard ["f", "c", "p", "d", "a", "s", "b"].contains(diceType) else {*//*
-			// not a valid type of dice
-			print("for `\(dicePool)`, `\(diceType)`, is not a valid dice type, the final character of an argument must be the type of dice")
-			break
-		}*/
-		// should be the number of dice of a type to roll
-		/*guard let dice = Int(String(dicePool.characters.dropLast())) else {
-			roll += rollDie(ofType: diceType) // if no number was provided, roll 1 die
-			continue
-		}*/
-		let dice = Int(String(dicePool.characters.dropLast())) ?? 1
-		roll += rollDice(numberOfDice: dice, ofType: diceType)
+	guard let diceType = dicePool.popLast() else {
+		print("the dice-type could not be determined from `\(arg)`")
+		usage()
+		exit(1)
 	}
-let end = Date()
-print("parse: \(end.timeIntervalSince(start))")
+
+	if let num = Int(String(dicePool)) {
+		rollDice(number: num, ofType: diceType)
+	} else if dicePool.isEmpty {
+		rollDice(number: 1, ofType: diceType)
+	} else {
+		print("the dice number could not be determined from `\(arg)`")
+		usage()
+	}
+}
 
 // count/calc each die face and output
 output()
-let end0 = Date()
-print("total: \(end0.timeIntervalSince(start0))")
